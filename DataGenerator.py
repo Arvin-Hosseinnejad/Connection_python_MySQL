6931e4404701f1d9ed4a4ad6bfba8695cb3c3ca7
Arvin Hosseinnejad <arvin.hosseinnejad@gmila.com>
2021-06-15 08:31:12 +0300

    DataGenerator.py

---
+++ DataGenerator.py
@@ -0,0 +1,15 @@
+import time
+import random
+
+def generator():
+    time.sleep(.1)
+    a = random.random()
+    b = random.random()
+    c = random.random()
+
+    return (a,b,c)
+
+if __name__ == "__main__":
+    for i in range(10):
+        result = generator()
+        print(result)
