494f78dec29f858c9fb3bd84f0da498f9ebb914f
Arvin Hosseinnejad <arvin.hosseinnejad@gmila.com>
2021-06-15 08:52:41 +0300

    DataGenerator.py

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,3 +1,11 @@
+""" 
+This is the module that includes a function 
+with which you can generate three random 
+numbers between 0 and 1. it will return these 
+three numbers within a tuple 
+ 
+"""
+
 import time
 import random
 
@@ -13,3 +21,5 @@ if __name__ == "__main__":
     for i in range(10):
         result = generator()
         print(result)
+
+
